165bc1d030f0d075d42ecc70255fe78414d184b1
tomas006 <2025183707@my.ipleiria.pt>
2025-10-29 00:30:58 +0000

    Add TicTacToe components and updates

---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,13 @@
+import Board from "./Board";
+
+function TictacToe(){
+    return (
+        <div>
+          <h1> Tic Tac Toe </h1>
+          <Board />
+        </div>
+    );
+
+}
+
+export default TictacToe;
